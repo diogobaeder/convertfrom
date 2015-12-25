db2d809b239bc842b05c1d1e1c6864c85aa412d5
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2015-12-25 19:54:43 -0200

    Moving ArgParser to its own module

--- convertfrom/main.py
+++ convertfrom/main.py
@@ -1,45 +1,14 @@
-import re
 import sys
 from decimal import Decimal
 
+from convertfrom.parser import ArgParser
 
-SOURCE_PATTERN = re.compile(r'(\d+)\s*(\w+)')
+
 UNIT_MAP = {
     ('m', 'meter', 'meters'): Decimal(1),
     'cm': Decimal(100),
     'mm': Decimal(1000),
 }
-
-
-class ArgParser:
-    def __init__(self):
-        self.remainings = None
-        self.destination_string = None
-        self.source_quantity = None
-        self.source_unit = None
-
-    def parse(self, args):
-        self.remainings = list(args)
-        source_parts = self.get_source_parts()
-        self.destination_string = self.remainings[0]
-        self.source_quantity = Decimal(str(float(source_parts[0])))
-        self.source_unit = source_parts[1]
-
-    def get_source_parts(self):
-        source_string = self.get_source_string()
-        matches = SOURCE_PATTERN.match(source_string)
-        source_parts = matches.groups()
-        return source_parts
-
-    def get_source_string(self):
-        source_tokens = []
-        while True:
-            token = self.remainings.pop(0)
-            if token == 'to':
-                break
-            source_tokens.append(token)
-        source_string = ' '.join(source_tokens)
-        return source_string
 
 
 def find_multiplier(unit):
---
+++ convertfrom/parser.py
@@ -0,0 +1,36 @@
+import re
+from decimal import Decimal
+
+
+SOURCE_PATTERN = re.compile(r'(\d+)\s*(\w+)')
+
+
+class ArgParser:
+    def __init__(self):
+        self.remainings = None
+        self.destination_string = None
+        self.source_quantity = None
+        self.source_unit = None
+
+    def parse(self, args):
+        self.remainings = list(args)
+        source_parts = self.get_source_parts()
+        self.destination_string = self.remainings[0]
+        self.source_quantity = Decimal(str(float(source_parts[0])))
+        self.source_unit = source_parts[1]
+
+    def get_source_parts(self):
+        source_string = self.get_source_string()
+        matches = SOURCE_PATTERN.match(source_string)
+        source_parts = matches.groups()
+        return source_parts
+
+    def get_source_string(self):
+        source_tokens = []
+        while True:
+            token = self.remainings.pop(0)
+            if token == 'to':
+                break
+            source_tokens.append(token)
+        source_string = ' '.join(source_tokens)
+        return source_string
