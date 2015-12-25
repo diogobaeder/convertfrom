8d92ec80b5893f42359cc80ffef94606a5aa131f
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2015-12-25 19:51:29 -0200

    Extracting ArgParser class

--- convertfrom/main.py
+++ convertfrom/main.py
@@ -11,6 +11,37 @@ UNIT_MAP = {
 }
 
 
+class ArgParser:
+    def __init__(self):
+        self.remainings = None
+        self.destination_string = None
+        self.source_quantity = None
+        self.source_unit = None
+
+    def parse(self, args):
+        self.remainings = list(args)
+        source_parts = self.get_source_parts()
+        self.destination_string = self.remainings[0]
+        self.source_quantity = Decimal(str(float(source_parts[0])))
+        self.source_unit = source_parts[1]
+
+    def get_source_parts(self):
+        source_string = self.get_source_string()
+        matches = SOURCE_PATTERN.match(source_string)
+        source_parts = matches.groups()
+        return source_parts
+
+    def get_source_string(self):
+        source_tokens = []
+        while True:
+            token = self.remainings.pop(0)
+            if token == 'to':
+                break
+            source_tokens.append(token)
+        source_string = ' '.join(source_tokens)
+        return source_string
+
+
 def find_multiplier(unit):
     for key, value in UNIT_MAP.items():
         if unit == key or (isinstance(key, (tuple, list)) and unit in key):
@@ -19,24 +50,15 @@ def find_multiplier(unit):
 
 
 def convert(args):
-    args = list(args)
-    source_tokens = []
-    while True:
-        token = args.pop(0)
-        if token == 'to':
-            break
-        source_tokens.append(token)
-    source_string = ' '.join(source_tokens)
-    destination_string = args[0]
-    matches = SOURCE_PATTERN.match(source_string)
-    source_parts = matches.groups()
-    source_quantity = Decimal(str(float(source_parts[0])))
-    source_unit = source_parts[1]
-    destination_multiplier = find_multiplier(destination_string)
-    source_multiplier = find_multiplier(source_unit)
-    result = source_quantity / source_multiplier * destination_multiplier
+    parser = ArgParser()
+    parser.parse(args)
 
-    return '{}{}'.format(result, destination_string)
+    destination_multiplier = find_multiplier(parser.destination_string)
+    source_multiplier = find_multiplier(parser.source_unit)
+    result = (
+        parser.source_quantity / source_multiplier * destination_multiplier)
+
+    return '{}{}'.format(result, parser.destination_string)
 
 
 def main():
